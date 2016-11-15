34a49818fad7ebcebdcbb1fdfbd461e150c8e74b
AndreaAraujo <andreaaraujocuquejo@gmail.com>
2016-11-15 20:28:14 +0100

    actividad

--- HTML-GYMWEB/model/model.php
+++ HTML-GYMWEB/model/model.php
@@ -5,4 +5,7 @@ include_once __DIR__."/../model/Usuario.php";
 include_once __DIR__."/../model/UsuarioMapper.php";
 include_once __DIR__."/../model/Ejercicio.php";
 include_once __DIR__."/../model/EjercicioMapper.php";
+include_once __DIR__."/../model/Actividad.php";
+include_once __DIR__."/../model/ActividadMapper.php";
+
 ?>
