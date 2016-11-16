e3035901a31f7629d926233f18f29df7c7f77d4c
Brais <braissg13@gmail.com>
2016-11-16 11:07:29 +0100

    Add files via upload

--- HTML-GYMWEB/model/Actividad.php
+++ HTML-GYMWEB/model/Actividad.php
@@ -1,25 +1,14 @@
 <?php
-
-
 include_once __DIR__."/../conexion/bdConexion.php";
 include_once __DIR__."/../model/ActividadMapper.php";
-
 class Actividad {
-
   protected $idActividad;
-
   protected $nomActividad;
-
   protected $totalPlazas;
-
   protected $descripAct;
-
   protected $fecha;
-
   protected $plazasOcupadas;
-
   protected $imagenAct;
-
   /*
     Constructor de la Actividad
   */
@@ -32,75 +21,57 @@ class Actividad {
     $this->plazasOcupadas = $plazasOcupadas;
     $this->imagenAct = $imagenAct;
   }
-
   public function getIdActividad() {
     return $this->idActividad;
   }
-
   public function setIdActividad($idActividad) {
-    $this->$idActividad = $idActividad;
+    $this->idActividad = $idActividad;
   }
-
   public function getNomActividad() {
     return $this->nomActividad;
   }
-
   public function setNomActividad($nomActividad) {
-    $this->nomAct = $nomActividad;
+    $this->nomActividad = $nomActividad;
   }
-
   public function getTotalPlazas() {
     return $this->totalPlazas;
   }
-
   public function setTotalPlazas($totalPlazas) {
-    $this->numPl = $totalPlazas;
+    $this->totalPlazas = $totalPlazas;
   }
-
   public function getDescripActividad() {
     return $this->descripAct;
   }
-
-  public function setDescripActividad($descAct) {
-    $this->descAct = $descripAct;
+  public function setDescripActividad($descripAct) {
+    $this->descripAct = $descripAct;
   }
-
-
   public function getFecha() {
     return $this->fecha;
   }
-
   public function setFecha($fecha) {
-    $this->dateAct = $fecha;
+    $this->fecha = $fecha;
   }
-
   public function getPlazasOcupadas() {
     return $this->plazasOcupadas;
   }
-
   public function setPlazasOcupadas($plazasOcupadas) {
-    $this->dateAct = $plazasOcupadas;
+    $this->plazasOcupadas = $plazasOcupadas;
   }
-
   public function getImagenActividad() {
     return $this->imagenAct;
   }
-
   public function setImagenActividad($imagenAct) {
-    $this->imagen = $imagenAct;
+    $this->imagenAct = $imagenAct;
   }
-
   /*Obtener todos las Actividades*/
   public static function getAllActividades()
     {
         return $resultado = ActividadMapper::findAll();
     }
-
     /* Guardamos una Actividad en la BD*/
   public static function guardarActividad($actividad){
     return ActividadMapper::guardarActividad($actividad);
   }
-
   /*Comprobacion existe Actividad... Si existe actividad devuelve Objeto Actividad*/
   public static function obtenerDatos($idActividad) {
     if ($idActividad) {
@@ -113,42 +84,28 @@ class Actividad {
             return "ERROR, no existe la actividad";
         }
   }
-
-
-
   /*Comprobamos si se puede registrar la Actividad. Si se puede retornamos un TRUE*/
- public static function registroValido($nomActividad,$totalPlazas,$descripAct,  $plazasOcupadas){
+ public static function registroValido($nomActividad,$descripAct){
       $error = array();
       if (strlen($nomActividad) < 3 || strlen($nomActividad) > 30) {
 	     $error["nomActividad"] = "El nombre de la Actividad debe tener entre 3 y 30 caracteres.";
       }
-      if (strlen($totalPlazas) < 1 || strlen($totalPlazas) > 2) {
-       $error["totalPlazas"] = "El numero total de plazas de la Actividad debe tener entre 1 y 2 digitos.";
-      }
-      if (strlen($descripAct) < 5 || strlen($descripAct) > 300) {
+      if (strlen($descripAct) < 5 || strlen($descripAct) > 500) {
 	     $error["descripAct"] = "La descripcion de la Actividad debe tener entre 5 y 300 caracteres.";
-      }
-      if (strlen($plazasOcupadas) < 1 || strlen($plazasOcupadas) > 2) {
-       $error["numPl"] = "El numero de plazas ocupadas de la Actividad debe tener entre 1 y 2 digitos.";
       }
       if (sizeof($error)>0){
 	     echo "No se puede resgistrar la Actividad por los siguientes motivos: ";
        print_r(array_values($error));
-
       }
       if (sizeof($error)==0){
        return true;
       }
-
   }
-
   public static function update($idActividad, $nomActividad, $totalPlazas, $descripAct, $fecha, $plazasOcupadas, $imagenAct){
       ActividadMapper::update($idActividad, $nomActividad,$totalPlazas,$descripAct,$fecha, $plazasOcupadas, $imagenAct);
   }
-
   public static function delete($idActividad){
       ActividadMapper::delete($idActividad);
   }
-
 }
 ?>
--- HTML-GYMWEB/model/ActividadMapper.php
+++ HTML-GYMWEB/model/ActividadMapper.php
@@ -2,7 +2,6 @@
 include_once __DIR__."/../conexion/bdConexion.php";
 
 class ActividadMapper{
-
     /*Buscamos todos las Actividades*/
     public static function findAll()
     {
@@ -10,7 +9,6 @@ class ActividadMapper{
         $resultado = mysqli_query($connect, 'SELECT * FROM actividad');
         return $resultado;
     }
-
     /*Buscamos si existe una Actividad por su Nombre, devolvemos true si existe*/
     public static function existeActividad($nomActividad) {
         global $connect;
@@ -20,17 +18,13 @@ class ActividadMapper{
             return true;
         }
     }
-
       /* Guardamos una Actividad en la BD*/
     public static function guardarActividad($actividad){
       global $connect;
       $resultado = false;
-
-        $sqlcrear= "INSERT INTO actividad (idActividad, nomActividad,totalPlazas,descripAct,fecha, plazasOcupadas, imagenAct) VALUES ('";
+        $sqlcrear= "INSERT INTO actividad (nomActividad,totalPlazas,descripAct,fecha, plazasOcupadas, imagenAct)VALUES ('";
       $sqlcrear = $sqlcrear.$actividad->getNomActividad()."','".$actividad->getTotalPlazas()."','".$actividad->getDescripActividad()."','".$actividad->getFecha()."','".$actividad->getPlazasOcupadas()."','".$actividad->getImagenActividad()."')";
-
         $resultado = mysqli_query($connect, $sqlcrear);
-
        return $resultado;
     }
     /*Cogemos todos los datos de una Actividad buscandolo por su ID y devolvemos un objeto Actividad*/
@@ -39,15 +33,12 @@ class ActividadMapper{
         $resultado = mysqli_query($connect, 'SELECT * FROM actividad WHERE idActividad ="'.$idActividad.'"');
         if (mysqli_num_rows($resultado) > 0) {
             $row = mysqli_fetch_assoc($resultado);
-
             $actividad= new Actividad($row['idActividad'],$row['nomActividad'],$row['totalPlazas'],$row['descripAct'],$row['fecha'],$row['plazasOcupadas'],$row['imagenAct']);
-
             return $actividad;
         } else {
             return NULL;
         }
     }
-
      /*Mira si la Actividad es valido y devuelve true.*/
     public static function esValidoActividad($idActividad) {
         global $connect;
@@ -57,23 +48,16 @@ class ActividadMapper{
             return true;
         }
     }
-
-
     public static function update($idActividad,$nomActividad,$totalPlazas,$descripAct, $fecha, $plazasOcupadas, $imagenAct)
     {
         global $connect;
-
         $resultado = mysqli_query($connect, "UPDATE actividad SET nomActividad=\"$nomActividad\", totalPlazas =\"$totalPlazas\", descripAct =\"$descripAct\",fecha=\"$fecha\", plazasOcupadas= \"$plazasOcupadas\", imagen=\"$imagenAct\" WHERE idActividad=\"$idActividad\"");
            return $resultado;
-
     }
-
     public static function delete($idActividad){
         global $connect;
-
         $resultado = mysqli_query($connect, "DELETE FROM actividad WHERE idActividad=\"$idActividad\"");
         return $resultado;
     }
-
 }
 ?>
