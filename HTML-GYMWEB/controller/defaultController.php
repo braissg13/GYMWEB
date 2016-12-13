ce428ddbbe07102bd6bbbc205b0dd9cb99aaa329
AndreaAraujo <andreaaraujocuquejo@gmail.com>
2016-12-13 18:50:22 +0100

    reserva

--- HTML-GYMWEB/controller/defaultController.php
+++ HTML-GYMWEB/controller/defaultController.php
@@ -5,6 +5,7 @@
 	include_once("EjercicioController.php");
 	include_once("ActividadController.php");
 	include_once("TablaController.php");
+	include_once("ReservaController.php");
 //Llamamos alcontrolador y su accion
 	if(isset($_GET["controlador"]) && isset($_GET["accion"])){
 		$targetController = ucfirst($_GET["controlador"])."Controller";
