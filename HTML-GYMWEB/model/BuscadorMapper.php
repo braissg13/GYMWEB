e50819aa33c53cf3cc28f499c7598f5b92251731
Roman <romanallariz@gmail.com>
2016-12-23 18:39:51 +0100

    cambios buscador

--- HTML-GYMWEB/model/BuscadorMapper.php
+++ HTML-GYMWEB/model/BuscadorMapper.php
@@ -3,11 +3,11 @@ require_once(__DIR__."/../conexion/bdConexion.php");
 
 
 class BuscadorMapper{
-    /*Buscamos todos las Actividades*/
+    /*Buscamos las Actividades con la cadena del buscador*/
     public static function findActBySearch($busq){
         global $connect;
-        $resultado = mysqli_query($connect, 'SELECT * FROM actividad WHERE nomActividad like '%$busq%'');
-        if (mysqli_num_rows($resultado) > 0) {
+        $resultado = mysqli_query($connect, 'SELECT * FROM actividad WHERE nomActividad like %$busq%');
+        if ($resultado) {
             $row = mysqli_fetch_assoc($resultado);
             $actividad= new Actividad($row['idActividad'],$row['nomActividad'],$row['totalPlazas'],$row['descripAct'],$row['fecha'],$row['plazasOcupadas'],$row['imagenAct']);
             return $actividad;
